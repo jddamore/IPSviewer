795d81b53a333cd7597074af857ec9b485338ee6
Daniel Lorigan <daniellrgn@gmail.com>
2025-01-09 14:32:10 -0800

    Fix type warning for demo mode store refs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,7 @@
   import Demo from '$lib/components/viewer/Demo.svelte';
   
   import { SHOW_VIEWER_DEMO } from "$lib/config";
+  import { type Writable } from "svelte/store";
   import { getContext } from 'svelte';
 
   let shlContents: Bundle[] = [];
@@ -32,7 +33,7 @@
     }
   }
 
-  let displayMode = getContext("displayMode");
+  let displayMode: Writable<string> = getContext("displayMode");
 
   // Status display logic
   let status = "Retrieving contents...";
